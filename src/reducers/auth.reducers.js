7a2421894e7d5036f937f02fb8d0de48d38d9489
Talha Patel <talhapatel.tp@gmail.com>
2021-05-28 10:29:47 +0530

    create auth reducer

--- src/reducers/auth.reducers.js
+++ src/reducers/auth.reducers.js
@@ -5,7 +5,7 @@ const initState = {
   user: {
     username: "",
     email: "",
-    role: [],
+    roles: [],
   },
   authenticate: false,
   authenticating: false,
@@ -15,7 +15,6 @@ const initState = {
 };
 
 export default (state = initState, action) => {
-  console.log(action);
   switch (action.type) {
     case authConstants.LOGIN_REQUEST:
       state = {
